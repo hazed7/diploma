1d77a7c06b15f15377fda37516289beb5ee120ed
Alexander Kostin <iheartapplejr@gmail.com>
2025-03-15 02:26:02 +0300

    chore: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,13 @@ export default tseslint.config(
 			'@typescript-eslint/no-unsafe-call': 'warn',
 			'@typescript-eslint/no-unsafe-assignment': 'warn',
 			'@typescript-eslint/no-unsafe-member-access': 'warn',
+			'@typescript-eslint/no-unsafe-return': 'warn',
+			'@typescript-eslint/require-await': 'warn',
+			'@typescript-eslint/restrict-template-expressions': 'warn',
+			'@typescript-eslint/no-base-to-string': 'warn',
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'@typescript-eslint/no-redundant-type-constituents': 'warn',
+			'@typescript-eslint/no-unsafe-enum-comparison': 'warn',
 			'prettier/prettier': ['error', {
 				useTabs: true,
 				singleQuote: false
